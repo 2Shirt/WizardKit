110084fce603540759c6f220604fb1bcc005a1dc
Alan Mason <1923621+2Shirt@users.noreply.github.com>
2017-12-03 19:54:44 -0800

    Added FirefoxExtensions to update.py

--- .bin/Scripts/update_kit.py
+++ .bin/Scripts/update_kit.py
@@ -67,6 +67,7 @@ if __name__ == '__main__':
         try_and_print(message='Classic Start Skin...', function=update_classic_start_skin, other_results=other_results, width=40)
         try_and_print(message='Du...', function=update_du, other_results=other_results, width=40)
         try_and_print(message='Everything...', function=update_everything, other_results=other_results, width=40)
+        try_and_print(message='FirefoxExtensions...', function=update_firefox_ublock_origin, other_results=other_results, width=40)
         try_and_print(message='PuTTY...', function=update_putty, other_results=other_results, width=40)
         try_and_print(message='Notepad++...', function=update_notepadplusplus, other_results=other_results, width=40)
         try_and_print(message='TreeSizeFree...', function=update_treesizefree, other_results=other_results, width=40)
