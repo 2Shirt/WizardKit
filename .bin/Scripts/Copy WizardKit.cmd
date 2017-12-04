45dc982f446aedb12eb773f7485c64e6bb65592a
Alan Mason <1923621+2Shirt@users.noreply.github.com>
2017-12-03 17:45:18 -0800

    Don't copy SDIO or Update Kit.cmd

--- .bin/Scripts/Copy WizardKit.cmd
+++ .bin/Scripts/Copy WizardKit.cmd
@@ -86,7 +86,7 @@ set _sources=%_sources% "%source%\README.md"
 set _sources=%_sources% "%source%\System Checklist.cmd"
 set _sources=%_sources% "%source%\System Diagnostics.cmd"
 set _sources=%_sources% "%source%\User Checklist.cmd"
-start "" /wait "%fastcopy%" %fastcopy_args% /exclude="Snappy Driver Installer.cmd;*.exe" %_sources% /to="%dest%\"
+start "" /wait "%fastcopy%" %fastcopy_args% /exclude="Snappy Driver Installer*;Update Kit*;*.exe" %_sources% /to="%dest%\"
 start "" /wait "%fastcopy%" %fastcopy_args% "%source%\Installers\Extras\Office\Adobe Reader DC.exe" /to="%dest%\Installers\Extras\Office\"
 
 :Ninite
