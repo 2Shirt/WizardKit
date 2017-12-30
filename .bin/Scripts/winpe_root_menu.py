b441738950ccebd3dae37dbf0c301fa881aeccf3
2Shirt <1923621+2Shirt@users.noreply.github.com>
2017-12-30 12:05:24 -0700

    Fixed opening logs in WinPE

--- .bin/Scripts/winpe_root_menu.py
+++ .bin/Scripts/winpe_root_menu.py
@@ -7,6 +7,8 @@ import sys
 os.chdir(os.path.dirname(os.path.realpath(__file__)))
 sys.path.append(os.getcwd())
 from functions.winpe_menus import *
+# Fix 7-Zip name
+TOOLS['SevenZip'].pop('64')
 init_global_vars()
 set_title('{}: Root Menu'.format(KIT_NAME_FULL))
 global_vars['LogFile'] = r'{LogDir}\WinPE.log'.format(**global_vars)
