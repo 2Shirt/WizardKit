6d5abd3b92343cf0f8c8c5811c7d65a8a460a14a
2Shirt <1923621+2Shirt@users.noreply.github.com>
2018-02-21 14:02:53 -0700

    Update launchers.py
    Bump current IRST version

--- .bin/Scripts/settings/launchers.py
+++ .bin/Scripts/settings/launchers.py
@@ -294,8 +294,8 @@ LAUNCHERS = {
         'Intel RST (Current Release)': {
             'L_TYPE': 'Executable',
             'L_PATH': '_Drivers\Intel RST',
-            'L_ITEM': 'SetupRST_15.8.exe',
-            'L_7ZIP': 'SetupRST_15.8.exe',
+            'L_ITEM': 'SetupRST_15.9.exe',
+            'L_7ZIP': 'SetupRST_15.9.exe',
             },
         'Intel RST (Previous Releases)': {
             'L_TYPE': 'Folder',
