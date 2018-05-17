3114b72178cf821234ebe5becb8792d10c30001e
2Shirt <1923621+2Shirt@users.noreply.github.com>
2018-05-17 17:57:45 -0600

    Add ticket toggle var and get_simple_string func
    * get_simple_string() will be used for backup prefixes

--- .bin/Scripts/functions/common.py
+++ .bin/Scripts/functions/common.py
@@ -196,6 +196,8 @@ def extract_item(item, filter='', silent=False):
 
 def get_ticket_number():
     """Get TicketNumber from user, save in LogDir, and return as str."""
+    if not ENABLED_TICKET_NUMBERS:
+        return None
     ticket_number = None
     while ticket_number is None:
         _input = input('Enter ticket number: ')
@@ -207,6 +209,15 @@ def get_ticket_number():
             with open(out_file, 'w', encoding='utf-8') as f:
                 f.write(ticket_number)
     return ticket_number
+
+def get_simple_string(prompt='Enter string'):
+    """Get string from user (only alphanumeric/space chars) and return as str."""
+    simple_string = None
+    while simple_string is None:
+        _input = input('{}: '.format(prompt))
+        if re.match(r'^(\w|-| )+$', _input, re.ASCII):
+            simple_string = _input.strip()
+    return simple_string
 
 def human_readable_size(size, decimals=0):
     """Convert size in bytes to a human-readable format and return a str."""
