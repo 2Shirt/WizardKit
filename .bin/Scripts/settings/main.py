b6e9e447c5868b06b839cf7cd9b659844ae697f7
2Shirt <2xShirt@gmail.com>
2018-12-27 22:45:49 -0700

    Fixed main.py

--- .bin/Scripts/settings/main.py
+++ .bin/Scripts/settings/main.py
@@ -5,7 +5,7 @@ ENABLED_OPEN_LOGS = False
 ENABLED_TICKET_NUMBERS = False
 ENABLED_UPLOAD_DATA = False
 HW_OVERRIDES_FORCED = False
-HW_OVERRIDES_LIMITED = True        # If True this disables HW_OVERRIDE_FORCED
+HW_OVERRIDES_LIMITED = True                 # If True this disables HW_OVERRIDE_FORCED
 
 # STATIC VARIABLES (also used by BASH and BATCH files)
 ## NOTE: There are no spaces around the = for easier parsing in BASH and BATCH
@@ -15,15 +15,15 @@ KIT_NAME_FULL='WizardKit'
 KIT_NAME_SHORT='WK'
 SUPPORT_MESSAGE='Please let 2Shirt know by opening an issue on GitHub'
 # Live Linux
-MPRIME_LIMIT='7'               # of minutes to run Prime95 during hw-diags
+MPRIME_LIMIT='7'                            # of minutes to run Prime95 during hw-diags
 ROOT_PASSWORD='Abracadabra'
 TECH_PASSWORD='Abracadabra'
 # Server IP addresses
 OFFICE_SERVER_IP='10.0.0.10'
 QUICKBOOKS_SERVER_IP='10.0.0.10'
 # Time Zones
-LINUX_TIME_ZONE='America/Denver'       # See 'timedatectl list-timezones' for valid values
-WINDOWS_TIME_ZONE='Mountain Standard Time' # See 'tzutil /l' for valid values
+LINUX_TIME_ZONE='America/Denver'            # See 'timedatectl list-timezones' for valid values
+WINDOWS_TIME_ZONE='Mountain Standard Time'  # See 'tzutil /l' for valid values
 # WiFi
 WIFI_SSID='SomeWiFi'
 WIFI_PASSWORD='Abracadabra'
@@ -34,57 +34,57 @@ WIFI_PASSWORD='Abracadabra'
 ##     user/password for all of those shares.
 BACKUP_SERVERS = [
   {   'IP':     '10.0.0.10',
-    'Name':   'ServerOne',
+    'Name':     'ServerOne',
     'Mounted':  False,
-    'Share':  'Backups',
-    'User':   'restore',
-    'Pass':   'Abracadabra',
+    'Share':    'Backups',
+    'User':     'restore',
+    'Pass':     'Abracadabra',
     'RW-User':  'backup',
     'RW-Pass':  'Abracadabra',
   },
   {   'IP':     '10.0.0.11',
-    'Name':   'ServerTwo',
+    'Name':     'ServerTwo',
     'Mounted':  False,
-    'Share':  'Backups',
-    'User':   'restore',
-    'Pass':   'Abracadabra',
+    'Share':    'Backups',
+    'User':     'restore',
+    'Pass':     'Abracadabra',
     'RW-User':  'backup',
     'RW-Pass':  'Abracadabra',
   },
 ]
 CRASH_SERVER = {
-  'Name':     'CrashServer',
-  'Url':      '',
-  'User':     '',
-  'Pass':     '',
+  'Name':       'CrashServer',
+  'Url':        '',
+  'User':       '',
+  'Pass':       '',
 }
 OFFICE_SERVER = {
-  'IP':       OFFICE_SERVER_IP,
-  'Name':     'ServerOne',
+  'IP':         OFFICE_SERVER_IP,
+  'Name':       'ServerOne',
   'Mounted':    False,
-  'Share':    'Office',
-  'User':     'restore',
-  'Pass':     'Abracadabra',
+  'Share':      'Office',
+  'User':       'restore',
+  'Pass':       'Abracadabra',
   'RW-User':    'backup',
   'RW-Pass':    'Abracadabra',
 }
 QUICKBOOKS_SERVER = {
-  'IP':       QUICKBOOKS_SERVER_IP,
-  'Name':     'ServerOne',
+  'IP':         QUICKBOOKS_SERVER_IP,
+  'Name':       'ServerOne',
   'Mounted':    False,
-  'Share':    'QuickBooks',
-  'User':     'restore',
-  'Pass':     'Abracadabra',
+  'Share':      'QuickBooks',
+  'User':       'restore',
+  'Pass':       'Abracadabra',
   'RW-User':    'backup',
   'RW-Pass':    'Abracadabra',
 }
 WINDOWS_SERVER = {
-  'IP':       '10.0.0.10',
-  'Name':     'ServerOne',
+  'IP':         '10.0.0.10',
+  'Name':       'ServerOne',
   'Mounted':    False,
-  'Share':    'Windows',
-  'User':     'restore',
-  'Pass':     'Abracadabra',
+  'Share':      'Windows',
+  'User':       'restore',
+  'Pass':       'Abracadabra',
   'RW-User':    'backup',
   'RW-Pass':    'Abracadabra',
 }
