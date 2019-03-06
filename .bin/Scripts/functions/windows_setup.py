8d8399c9aad20decb8fab16cb4d86dff8627a2fc
2Shirt <2xShirt@gmail.com>
2019-03-05 18:26:08 -0700

    Moved windows setup settings to separate file

--- .bin/Scripts/functions/windows_setup.py
+++ .bin/Scripts/functions/windows_setup.py
@@ -2,39 +2,7 @@
 
 from functions.data import *
 from functions.disk import *
-
-
-# STATIC VARIABLES
-WINDOWS_VERSIONS = [
-  {'Name': 'Windows 7 Home Basic',
-    'Image File': 'Win7',
-    'Image Name': 'Windows 7 HOMEBASIC'},
-  {'Name': 'Windows 7 Home Premium',
-    'Image File': 'Win7',
-    'Image Name': 'Windows 7 HOMEPREMIUM'},
-  {'Name': 'Windows 7 Professional',
-    'Image File': 'Win7',
-    'Image Name': 'Windows 7 PROFESSIONAL'},
-  {'Name': 'Windows 7 Ultimate',
-    'Image File': 'Win7',
-    'Image Name': 'Windows 7 ULTIMATE'},
-
-  {'Name': 'Windows 8.1',
-    'Image File': 'Win8',
-    'Image Name': 'Windows 8.1',
-    'CRLF': True},
-  {'Name': 'Windows 8.1 Pro',
-    'Image File': 'Win8',
-    'Image Name': 'Windows 8.1 Pro'},
-
-  {'Name': 'Windows 10 Home',
-    'Image File': 'Win10',
-    'Image Name': 'Windows 10 Home',
-    'CRLF': True},
-  {'Name': 'Windows 10 Pro',
-    'Image File': 'Win10',
-    'Image Name': 'Windows 10 Pro'},
-  ]
+from settings.windows_setup import *
 
 
 def find_windows_image(windows_version):
