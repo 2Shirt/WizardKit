6a821de0b56c5b4fc8b1a0c10c56ba2a6a993273
2Shirt <2xShirt@gmail.com>
2019-06-04 18:58:24 -0600

    Added LibreOffice settings

--- .bin/Scripts/settings/setup.py
+++ .bin/Scripts/settings/setup.py
@@ -135,6 +135,16 @@ SETTINGS_EXPLORER_USER = {
     },
   }
 
+# LibreOffice
+LIBREOFFICE_XCU_DATA = '''<?xml version="1.0" encoding="UTF-8"?>
+<oor:items xmlns:oor="http://openoffice.org/2001/registry" xmlns:xs="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+<item oor:path="/org.openoffice.Setup/Office/Factories/org.openoffice.Setup:Factory['com.sun.star.presentation.PresentationDocument']"><prop oor:name="ooSetupFactoryDefaultFilter" oor:op="fuse"><value>Impress MS PowerPoint 2007 XML</value></prop></item>
+<item oor:path="/org.openoffice.Setup/Office/Factories/org.openoffice.Setup:Factory['com.sun.star.sheet.SpreadsheetDocument']"><prop oor:name="ooSetupFactoryDefaultFilter" oor:op="fuse"><value>Calc MS Excel 2007 XML</value></prop></item>
+<item oor:path="/org.openoffice.Setup/Office/Factories/org.openoffice.Setup:Factory['com.sun.star.text.TextDocument']"><prop oor:name="ooSetupFactoryDefaultFilter" oor:op="fuse"><value>MS Word 2007 XML</value></prop></item>
+<item oor:path="/org.openoffice.Office.Common/Save/Document"><prop oor:name="WarnAlienFormat" oor:op="fuse"><value>false</value></prop></item>
+</oor:items>
+'''
+
 # Visual C++ Runtimes
 VCR_REDISTS = [
   {'Name': 'Visual C++ 2010 x32...',
