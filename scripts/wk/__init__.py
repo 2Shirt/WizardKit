4cc54011b74d65b0c518c9791f35bd086273efba
2Shirt <2xShirt@gmail.com>
2019-07-07 22:02:48 -0600

    Added version check

--- scripts/wk/__init__.py
+++ scripts/wk/__init__.py
@@ -1,4 +1,7 @@
 '''WizardKit: wk module init'''
+# vim: sts=2 sw=2 ts=2
+
+import sys
 
 from wk import cfg
 from wk import exe
@@ -9,3 +12,21 @@ from wk import net
 from wk import os
 from wk import std
 from wk import sw
+
+# Check env
+if sys.version_info < (3, 5):
+  # Unsupported
+  raise RuntimeError(
+    'This package is unsupported on Python {major}.{minor}'.format(
+      **sys.version_info,
+      ))
+if sys.version_info < (3, 7):
+  # Untested
+  raise UserWarning(
+    'Python {major}.{minor} is untested for this package'.format(
+      **sys.version_info,
+      ))
+
+
+if __name__ == '__main__':
+  print("This file is not meant to be called directly.")
