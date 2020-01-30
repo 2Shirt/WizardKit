dc6dcfb84528f5f28f683c11603a3dbac7278298
2Shirt <2xShirt@gmail.com>
2020-01-30 13:25:46 -0700

    Revert "Avoid crash during SMART self-test"
    This reverts commit cc8c0992f659c89127c24baf420313d0a9f3decc.

--- scripts/wk/hw/obj.py
+++ scripts/wk/hw/obj.py
@@ -368,12 +368,8 @@ class Disk(BaseObj):
     try:
       details = self.smartctl['ata_smart_data']['self_test']
     except (KeyError, TypeError):
-      # Assuming disk lacks SMART support, ignore and return nearly empty dict.
+      # Assuming disk lacks SMART support, ignore and return empty dict.
       pass
-
-    # Ensure status is present even if empty
-    if 'status' not in details:
-      details['status'] = {}
 
     # Done
     return details
