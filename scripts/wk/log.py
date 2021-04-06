9eb782e2b7a64aa7faa7bb380e8ee62b794ce7c6
2Shirt <2xShirt@gmail.com>
2021-04-06 17:06:48 -0600

    Remove macOS override to default log path
    Isn't needed since we're setting $HOME and including a symlink.
    i.e. /var/root/Logs --> /Volumes/RAM_Disk/Logs

--- scripts/wk/log.py
+++ scripts/wk/log.py
@@ -13,10 +13,7 @@ from wk.io import non_clobber_path
 
 
 # STATIC VARIABLES
-if os.path.exists('/.wk-live-macos'):
-  # Workaround for live macOS env
-  DEFAULT_LOG_DIR = '/Volumes/RAM_Disk/Logs'
-elif os.name == 'nt':
+if os.name == 'nt':
   # Example: "C:\WK\1955-11-05\WizardKit"
   DEFAULT_LOG_DIR = (
     f'{os.environ.get("SYSTEMDRIVE", "C:")}/'
