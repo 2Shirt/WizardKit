cc95bbe68b18ae9de4b5475d22db960264a2d8d0
2Shirt <2xShirt@gmail.com>
2021-10-14 23:59:26 -0600

    Add embedded_python_env.py
    Allows for faster debugging when using the embedded version of Python.

---
+++ scripts/embedded_python_env.py
@@ -0,0 +1,13 @@
+"""WizardKit: Embedded Python helper.
+
+This saves the keystrokes needed to fix the path and import wk. To use:
+python.exe -i embedded_python_env.py
+"""
+# vim: sts=2 sw=2 ts=2
+
+import os
+import sys
+
+os.chdir(os.path.dirname(os.path.realpath(__file__)))
+sys.path.append(os.getcwd())
+import wk # pylint: disable=wrong-import-position
